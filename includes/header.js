c77453513b89093dcaa5337383f9866d2144c391
klitsunova <kateryna.klitsunova@gmail.com>
2025-06-07 14:30:18 +0300

    Fix positioning and width in header

--- includes/header.js
+++ includes/header.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("header").innerHTML = `
         <div class="header-container">
-            <div class="header-decor-left"></div>
-            
+            <div class="header-background"></div>
+
             <div class="header-text">Кабинет скандинавской мифологии</div>
             <div class="header-decor-right">
+                <div class="header-background-right"></div>
                 <div class="header-image">
                     <img src="images/header-image.png" alt="Декоративное изображение">
                 </div>
